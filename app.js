78478e1b2234e0cf42fd74000456b6bcdc65af27
Luke Michalakis <michalakisluke@gmail.com>
2021-10-07 19:10:20 -0400

    <Build inquire function>

---
+++ app.js
@@ -0,0 +1,109 @@
+const inquirer = require('inquirer');
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+         type: 'input',
+         name: 'name',
+         message: 'What is the name of your project? (Required)',
+         validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } 
+            else {
+                console.log('Please enter the project name!');
+                return false;
+            }
+         }
+        },
+        {
+         type: 'input',
+         name: 'description',
+         message: 'Enter a project description. (Required)',
+         validate: descInput => {
+            if (descInput) {
+                return true;
+            } 
+            else {
+                console.log('Please enter a description!');
+                return false;
+            }
+         }
+        },
+        {
+         type: 'input',
+         name: 'installation',
+         message: 'Please enter any installation requirements for this project, or n/a if there are none.',
+         validate: instInput => {
+             if (instInput) {
+                return true;
+             } 
+             else {
+                console.log('Please enter a response!');
+                return false;
+             }
+            }
+        },
+        {
+         type: 'input',
+         name: 'usage',
+         message: 'Please enter usage instructions.',
+         validate: useInput => {
+             if (useInput) {
+                return true;
+             } 
+             else {
+                console.log('Please enter a usage instructions!');
+                return false;
+             }
+            }
+        },
+        {
+         type: 'list',
+         name: 'licenses',
+         message: 'Please choose a license for your project.',
+         choices: ['MIT', 'The Unlicense', 'ISC']
+        },
+        {
+         type: 'input',
+         name: 'contribute',
+         message: 'Please enter any contributors github usernames, seperated by commas.'
+        },
+        {
+         type: 'input',
+         name: 'test',
+         message: 'Please enter any testing requirements.'            
+        },
+        {
+         type: 'input',
+         name: 'email',
+         message: 'Please enter your email address',
+         validate: emailInput => {
+            if (emailInput) {
+               return true;
+            } 
+            else {
+               console.log('Please enter your email!');
+               return false;
+            }
+           }
+        },
+        {
+         type: 'input',
+         name: 'github',
+         message: 'Please enter your github username',
+         validate: gitInput => {
+            if (gitInput) {
+               return true;
+            } 
+            else {
+               console.log('Please enter your github username!');
+               return false;
+            }
+           }
+        }
+    ]);  
+};
+
+promptUser().then(function(answer){
+    console.log(answer);
+});
